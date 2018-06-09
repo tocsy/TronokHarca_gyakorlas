0ef8b23a2b023130f778611149903c27f598e772
Andrea Marton <andi.marton@gmail.com>
2018-06-09 12:46:20 +0200

    characters on screen and search field

--- js/got.js
+++ js/got.js
@@ -13,6 +13,12 @@ function successAjax(xhttp) {
     // itt a json content, benne a data változóban
     var userDatas = JSON.parse(xhttp.responseText);
     console.log(userDatas);
+    var alive = livingCharacters(userDatas);
+    console.log("élő karakterek: ", alive);
+    sortByName(alive)
+    console.log("élő karakterek név szerint rendezve: ", alive);
+    createHtmlElements(alive);
+
     /*
       Pár sorral lejebb majd ezt olvashatod:
       IDE ÍRD A FÜGGVÉNYEKET!!!!!! NE EBBE AZ EGY SORBA HANEM INNEN LEFELÉ!
@@ -29,3 +35,80 @@ getData('/json/characters.json', successAjax);
 
 // Live servert használd mindig!!!!!
 /* IDE ÍRD A FÜGGVÉNYEKET!!!!!! NE EBBE AZ EGY SORBA HANEM INNEN LEFELÉ! */
+
+function livingCharacters(characters) {
+    var live = [];
+
+    for (var i = 0; i < characters.length; i++) {
+        if (!characters[i].dead) {
+            live.push(characters[i]);
+        }
+    }
+    return live;
+}
+
+// function livingCharacters(characters) {
+//     var live = [];
+//     for (var i in characters) {
+//         if (!characters[i].dead) { //először ez volt: (characters[i].dead == '')itt lehetne ===-t is használni
+//             live.push(characters[i]);
+//         }
+//     }
+//     return live;
+// }
+
+// function sortByName(characters) {
+//     var i = characters.length - 1;
+//     var tmp;
+//     var swap = false;
+//     do {
+//         swap = false;
+//         for (var j = 0; j < i; j++) {
+//             if (characters[j].name > characters[j + 1].name) {
+//                 tmp = characters[j];
+//                 characters[j] = characters[j + 1];
+//                 characters[j + 1] = tmp;
+//                 swap = true;
+//             }
+//         }
+//         i--;
+//     } while (i >= 0 && swap)
+//     return characters;
+// }
+
+function sortByName(characters) {
+    characters = characters.sort(function (lho, rho) {
+        var result;
+
+        if (lho.name > rho.name) {
+            result = 1;
+        } else if (lho.name == rho.name) {
+            result = 0;
+        } else if (lho.name < rho.name) {
+            result = -1;
+        }
+        return result;
+    });
+};
+
+function createHtmlElements(characters) {
+    var canvasDivElement = document.querySelector("#canvas");
+
+    for (var i = 0; i < characters.length; i++) {
+        var cardDivElement = document.createElement('div');
+        cardDivElement.classList.add("card");
+        canvasDivElement.appendChild(cardDivElement);
+
+        var cardDivElementImage = document.createElement("img");
+        cardDivElement.appendChild(cardDivElementImage);
+        cardDivElementImage.src = characters[i].portrait;
+        cardDivElementImage.alt = characters[i].name;
+
+        var cardDivElementName = document.createElement("div");
+        cardDivElement.appendChild(cardDivElementName);
+        cardDivElementName.innerText = characters[i].name;
+
+
+    }
+
+};
